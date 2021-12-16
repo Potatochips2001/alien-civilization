8e44258e34b76411be09f4f2435b1eb462319301
Potatochips2001 <76017734+Potatochips2001@users.noreply.github.com>
2021-12-15 22:52:26 -0500

    added default values for andromeda

--- src/script.js
+++ src/script.js
@@ -1,46 +1,73 @@
-let stars, fractionStars, fractionLife, fractionIntelligent, fractionCommunicate, planetLifespan, communicationLifespan, alien, alienDensity;
-let galaxyDiameter, galaxyThickness;
-let intelligentSpecies, intelligentSpeciesDensity;
-
 document.addEventListener('keydown', (event) => {
   if (event.keyCode === 13) document.getElementById('btn').click();
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('galaxy-select').addEventListener('change', () => {
+    let galaxyOption = document.getElementById('galaxy-select').value;
+    switch (galaxyOption) {
+      case 'milky-way': {
+        document.getElementById('stars').value = '494,925,397,747';
+        document.getElementById('galaxy-diameter').value = '105,700';
+        document.getElementById('galaxy-thickness').value = '1,956.94';
+        break;
+      }
+      case 'andromeda': {
+        document.getElementById('stars').value = '2,848,596,690,773';
+        document.getElementById('galaxy-diameter').value = '220,000';
+        document.getElementById('galaxy-thickness').value = '2,600';
+        break;
+      }
+      default: {
+        return 1;
+      }
+    }
+  });
+});
+
 function getUIValue(_id) {
-  return document.getElementById(_id).value
+  return document.getElementById(_id).value;
 }
 
 function getAliens(s, fs, fl, fi, fc, pl, cl) {
   let n = s * fs * fl * fi * fc * (cl / pl);
-  return Math.floor(n)
+  return Math.floor(n);
 }
 
 function getIntelligent(s, fs, fl, fi, pl, cl) {
   let n = s * fs * fl * fi * (cl / pl);
-  return Math.floor(n)
+  return Math.floor(n);
 }
 
-function getIntelligentDensity(gd, gt) {
-  let volume = Math.pow(gd / 2, 2) * Math.PI * gt;
-  let d = (volume / intelligentSpecies / gt) ** 0.5;
-  return d
+function getIntelligentDensity(gd, gt, _aliens) {
+  let dIntelligent = (gd ** 2 + gt ** 2) ** 0.5 / _aliens;
+  return dIntelligent;
 }
 
-function getAlienDensity(gd, gt) {
-  let volume = Math.pow(gd/2, 2) * Math.PI * gt;
-  let d = (volume / alien / gt)**0.5;
-  return d
+function getAlienDensity(gd, gt, _aliens) {
+  let dAlien = (gd ** 2 + gt ** 2) ** 0.5 / _aliens;
+  return dAlien;
+}
+
+
+function ui2Num(_ui) {
+  let uiNumber = _ui.replace(/[^0-9.]/g, '');
+  return uiNumber;
 }
 
 function calculate() {
-  stars = getUIValue('stars'), fractionStars = getUIValue('fraction-stars'), fractionLife = getUIValue('fraction-life'),
-    fractionIntelligent = getUIValue('fraction-intelligent'), fractionCommunicate = getUIValue('fraction-communicate'),
-    planetLifespan = getUIValue('planet-lifespan'), communicationLifespan = getUIValue('communication-lifespan'),
-    galaxyDiameter = getUIValue('galaxy-diameter'), galaxyThickness = getUIValue('galaxy-thickness');
-  alien = getAliens(stars.replace(/[^0-9.]/g, ''), fractionStars.replace(/[^0-9.]/g, ''), fractionLife.replace(/[^0-9.]/g, ''), fractionIntelligent.replace(/[^0-9.]/g, ''), fractionCommunicate.replace(/[^0-9.]/g, ''), planetLifespan.replace(/[^0-9.]/g, ''), communicationLifespan.replace(/[^0-9.]/g, ''));
-  alienDensity = getAlienDensity(galaxyDiameter.replace(/[^0-9.]/g, ''), galaxyThickness.replace(/[^0-9.]/g, '')); //alien density regex
-  document.getElementById('result').innerText = `Alien civilizations N=${alien.toLocaleString()}\nOne civilization every ${alienDensity.toLocaleString()} lightyear(s)`;
-  intelligentSpecies = getIntelligent(stars.replace(/[^0-9.]/g, ''), fractionStars.replace(/[^0-9.]/g, ''), fractionLife.replace(/[^0-9.]/g, ''), fractionIntelligent.replace(/[^0-9.]/g, ''), planetLifespan.replace(/[^0-9.]/g, ''), communicationLifespan.replace(/[^0-9.]/g, ''));
-  intelligentSpeciesDensity = getIntelligentDensity(galaxyDiameter.replace(/[^0-9.]/g, ''), galaxyThickness.replace(/[^0-9.]/g, ''));
-  document.getElementById('result-2').innerText = `Planets with intelligent life ${intelligentSpecies.toLocaleString()}\nOne planet with intelligent life every ${intelligentSpeciesDensity.toLocaleString()} lightyear(s)`;
+  let stars = ui2Num(getUIValue('stars')), fStars = ui2Num(getUIValue('fraction-stars')), fLife = ui2Num(getUIValue('fraction-life')),
+    fIntelligent = ui2Num(getUIValue('fraction-intelligent')), fCommunicate = ui2Num(getUIValue('fraction-communicate')),
+    pLifespan = ui2Num(getUIValue('planet-lifespan')), cLifespan = ui2Num(getUIValue('communication-lifespan'));
+    galaxyDiameter = ui2Num(getUIValue('galaxy-diameter')), galaxyThickness = ui2Num(getUIValue('galaxy-thickness'));
+  let aliens = getAliens(stars, fStars, fLife, fIntelligent, fCommunicate, pLifespan, cLifespan),
+    alienDensity = getAlienDensity(galaxyDiameter, galaxyThickness, aliens);
+    intelligentLife = getIntelligent(stars, fStars, fLife, fIntelligent, pLifespan, cLifespan);
+    intelligentLifeDensity = getIntelligentDensity(galaxyDiameter, galaxyThickness, intelligentLife);
+  document.getElementById('result').innerText = `Alien civilizations ${aliens}
+  One alien civilization every ${alienDensity.toLocaleString()} lightyears
+  
+  Intelligent life ${intelligentLife.toLocaleString()}
+  One intelligent life planet every ${intelligentLifeDensity.toLocaleString()} lightyears`;
+  return 0;
 }
